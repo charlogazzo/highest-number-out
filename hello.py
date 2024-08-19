d316eecb6fe841262a3afc35f5793b666bfd42a1
Charles Nwofor <c.nwofor@hasenkamp.com>
2024-08-19 15:19:30 +0200

    add feature to deal cards to players and assign numerical values to each card

--- hello.py
+++ hello.py
@@ -1,12 +1,23 @@
 import pydealer
 from pydealer import (Deck, Stack, Card)
 
+card_numerical_values = {
+    "Ace": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "Jack": 11,
+    "Queen": 12,
+    "King": 13,
+    "Joker": 20
+}
+
 deck = pydealer.Deck()
-red_joker = Card("Joker", "Red")
-black_joker = Card("Joker", "Black")
-
-deck.add(red_joker)
-deck.add(black_joker)
-
-for card in deck:
-    print(card)
+red_joker = Card("Ace", "Spades")
+black_joker = Card("Ace", "Spades")
